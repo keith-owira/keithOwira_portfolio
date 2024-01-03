fd2b5d8dd714840a7ddc5326241f55eb7a79e8b8
keith-owira <owirakeith57@gmail.com>
2024-01-03 09:22:17 +0300

    SEO Update

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,26 @@
 </template>
 
 <script>
-
+  import { computed,reactive } from "vue"
+  import {useHead} from '@vueuse/head'
 export default {
   name: 'App',
+  
+  setup(){
+    const siteData = reactive({
+      title:'Keith Owira',
+      description:'Keith Owira portfolio|Software Engineer and Backend Developer with proven track record working with advanced data structures and algorithms '
+    })
+    useHead({
+      title:computed(()=>siteData.title),
+      meta:[
+        {
+          name:'description',
+          content:computed(()=>siteData.description),
+        },
+      ],
+    })
+  },
 
   data: () => ({
     //
--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import { createHead } from '@vueuse/head'
+const head = createHead();
 
 loadFonts()
 
@@ -11,4 +13,6 @@ createApp(App)
   .use(router)
   .use(store)
   .use(vuetify)
+  .use(head)
   .mount('#app')
+
